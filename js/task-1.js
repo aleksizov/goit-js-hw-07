7092c8594347c59e363049382216a38bfc6ea496
aleksizov <108980867+aleksizov@users.noreply.github.com>
2024-01-11 13:04:30 +0200

    task-1 removed errors

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,8 @@
-const categoryItems = document.querySelectorAll('.list-item');
-console.log(`Number of categories: ${categoryItems.length}`);
-categoryItems.forEach(item => {
-    const categoryTitle = item.textContent;
-    console.log(`Categories: ${categoryTitle}`);
-    const elementsCount = item.querySelectorAll('ul li').length;
-    console.log(`Elements: ${elementsCount}`);
+const categories = document.querySelectorAll('.list-item');
+console.log(`Number of categories: ${categories.length}`);
+categories.forEach(category => {
+  const categoryTitle = category.querySelector('h2').textContent;
+  console.log(`Category: ${categoryTitle}`);
+  const elementsCount = category.querySelectorAll('ul li').length;
+  console.log(`Elements: ${elementsCount}`);
 });
-
-// Запит item.querySelectorAll('.h2') неправильний і не буде працювати правильно, оскільки h2 - це тег, а не клас, тому точкову нотацію для класу не слід використовувати. Він має бути виправлений на item.querySelectorAll('ul li'), якщо метою є підрахунок елементів у кожній категорії, а не тегів h2.
